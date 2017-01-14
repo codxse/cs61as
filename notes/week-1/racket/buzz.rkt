11fd0c768922f40b0578f36fff2399cc7a9096a0
Nadiar AS <codxse@gmail.com>
2017-01-15 02:08:20 +0700

    using clojure

--- notes/week-1/racket/buzz.rkt
+++
@@ -1,5 +0,0 @@
-(define (buzz n)
-  (cond
-    ((equal? (remainder 7 n) 0) 'buzz)
-    ((member? 7 n) 'buzz)
-    (else n)))
--- notes/week-1/racket/plural.rkt
+++
@@ -1,4 +0,0 @@
-(define (plural wd)
-  (if (equal? 'y (last wd))
-    (word (butlast wd) 'ies)
-    (word wd 's)))
